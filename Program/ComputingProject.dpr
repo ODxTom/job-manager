efb4e57b27433a15648f67b95268849e81255b0d
ODxTom <43616660+ODxTom@users.noreply.github.com>
2018-09-26 18:03:14 +0100

    Add files via upload

---
+++ Program/ComputingProject.dpr
@@ -0,0 +1,14 @@
+program ComputingProject;
+
+uses
+  Vcl.Forms,
+  MainMenu in 'MainMenu.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
